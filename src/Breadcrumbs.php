026f9b195ea72c32db989ecd7ed44f1da36299af
carono <info@carono.ru>
2019-05-06 13:35:25 +0500

    refactor

--- src/Breadcrumbs.php
+++ src/Breadcrumbs.php
@@ -11,23 +11,38 @@ class Breadcrumbs
     public static $crumbsNamespace = 'app\breadcrumbs';
 
     /**
+     * @param $action
+     * @return string
+     */
+    protected static function actionToBreadcrumbClass($action)
+    {
+        $arr = explode('/', Inflector::camel2id($action->controller->getUniqueId()));
+        $arr[\count($arr) - 1] = Inflector::camelize(ucfirst($arr[\count($arr) - 1]));
+        return static::$crumbsNamespace . '\\' . implode('\\', $arr) . 'Crumbs';
+    }
+
+    protected static function actionToBreadcrumbMethod($action)
+    {
+        return 'crumb' . Inflector::camelize($action->id);
+    }
+
+    /**
      * @param \yii\base\Action $action
      * @param array $params
      * @return array|mixed
      */
     public static function formCrumbs($action, $params)
     {
-        $arr = explode('/', Inflector::camel2id($action->controller->getUniqueId()));
-        $arr[\count($arr) - 1] = Inflector::camelize(ucfirst($arr[\count($arr) - 1]));
-        $class = static::$crumbsNamespace . '\\' . implode('\\', $arr) . 'Crumbs';
-        $name = 'crumb' . Inflector::camelize($action->id);
-        if (method_exists($class, $name)) {
-            $reflectionMethod = new \ReflectionMethod($class, $name);
+        $class = static::actionToBreadcrumbClass($action);
+        $method = static::actionToBreadcrumbMethod($action);
+
+        if (class_exists($class) && method_exists($class, $method)) {
+            $reflectionMethod = new \ReflectionMethod($class, $method);
             $data = [];
             foreach ($reflectionMethod->getParameters() as $p) {
                 $data[] = $params[$p->getName()] ?? null;
             }
-            return array_filter(\call_user_func_array([$class, $name], $data));
+            return array_filter(\call_user_func_array([$class, $method], $data));
         }
 
         return [];

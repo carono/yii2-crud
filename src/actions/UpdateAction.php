5633f54bffae6ca4cdd682a793116fefc395841f
Касьянов <info@carono.ru>
2025-10-02 16:29:45 +0500

    refs

--- src/actions/UpdateAction.php
+++ src/actions/UpdateAction.php
@@ -6,13 +6,15 @@ use carono\yii2crud\CrudController;
 use carono\yii2crud\Event;
 use Yii;
 use yii\helpers\Html;
+use yii\db\ActiveRecord;
+use Closure;
 
 /**
  * Class UpdateAction
  *
  * @package carono\yii2crud\actions
  * @property CrudController $controller
- * @method getMessageOnUpdate(\yii\db\ActiveRecord $model)
+ * @method getMessageOnUpdate(ActiveRecord $model)
  */
 class UpdateAction extends Action
 {
@@ -22,32 +24,95 @@ class UpdateAction extends Action
     public $properties = [];
     public $standAloneSave = false;
 
+    public function redirect($model)
+    {
+        if (!$url = $this->getRedirectUrl($model)) {
+            return $this->controller->refresh();
+        }
+        if (Yii::$app->request->isPjax) {
+            return Yii::$app->response->redirect($url, 302, false);
+        }
+        return $this->controller->redirect($url);
+    }
+
+    public function renderView($model)
+    {
+        if ($this->view) {
+            return $this->render($this->view, compact('model'));
+        }
+        return $this->redirect($model);
+    }
+
     public function run()
     {
         $model = $this->findModel($this->modelClass ?: $this->controller->updateClass);
-        $this->controller->trigger(CrudController::EVENT_BEFORE_UPDATE_LOAD, new Event(['model' => $model, 'action' => $this]));
+
+        $this->applyProperties($model);
+
+        $this->triggerBeforeUpdateLoad($model);
+        if (!$model->load(Yii::$app->request->post()) && !$this->standAloneSave) {
+            return $this->renderView($model);
+        }
+        $this->triggerAfterUpdateLoad($model);
+
+        if (!$model->save()) {
+            $this->handleError($model);
+            return $this->renderView($model);
+        }
+
+        $this->handleSuccess($model);
+
+        return $this->redirect($model);
+    }
+
+    private function triggerBeforeUpdateLoad(ActiveRecord $model): void
+    {
+        $this->controller->trigger(CrudController::EVENT_BEFORE_UPDATE_LOAD, new Event([
+            'model' => $model,
+            'action' => $this
+        ]));
+    }
+
+    private function applyProperties(ActiveRecord $model): void
+    {
         foreach ($this->properties as $property => $value) {
             $model->$property = $value;
         }
-        if ($model->load(Yii::$app->request->post()) || $this->standAloneSave) {
-            $this->controller->trigger(CrudController::EVENT_AFTER_UPDATE_LOAD, new Event(['model' => $model, 'action' => $this]));
-            if ($model->save()) {
-                $this->controller->trigger(CrudController::EVENT_AFTER_UPDATE, new Event(['model' => $model, 'action' => $this]));
-                Yii::$app->session->setFlash('success', $this->getMessageOnUpdate($model));
-                if ($this->redirect instanceof \Closure) {
-                    $url = call_user_func($this->redirect, $model);
-                } else {
-                    return $this->controller->refresh();
-                }
-                if (Yii::$app->request->isPjax) {
-                    return Yii::$app->response->redirect($url, 302, false);
-                }
-                return $this->controller->redirect($url);
-            } else {
-                $this->controller->trigger(CrudController::EVENT_ERROR_UPDATE, new Event(['model' => $model, 'action' => $this]));
-            }
-            Yii::$app->session->setFlash('error', Html::errorSummary($model));
+    }
+
+    private function triggerAfterUpdateLoad(ActiveRecord $model): void
+    {
+        $this->controller->trigger(CrudController::EVENT_AFTER_UPDATE_LOAD, new Event([
+            'model' => $model,
+            'action' => $this
+        ]));
+    }
+
+    private function handleError(ActiveRecord $model): void
+    {
+        $this->controller->trigger(CrudController::EVENT_ERROR_UPDATE, new Event([
+            'model' => $model,
+            'action' => $this
+        ]));
+
+        Yii::$app->session->setFlash('error', Html::errorSummary($model));
+    }
+
+    private function handleSuccess(ActiveRecord $model)
+    {
+        $this->controller->trigger(CrudController::EVENT_AFTER_UPDATE, new Event([
+            'model' => $model,
+            'action' => $this
+        ]));
+
+        Yii::$app->session->setFlash('success', $this->getMessageOnUpdate($model));
+    }
+
+    private function getRedirectUrl(ActiveRecord $model)
+    {
+        if ($this->redirect instanceof Closure) {
+            return call_user_func($this->redirect, $model);
         }
-        return $this->render($this->view, ['model' => $model]);
+        return $this->redirect;
     }
 }
